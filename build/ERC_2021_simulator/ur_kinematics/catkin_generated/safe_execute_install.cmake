3c8ddc4b4c05a4ecee5ead5839973269eaaad301
Diego <df.rodriguezg@uniandes.edu.co>
2021-07-17 22:42:17 -0500

    Solucion problemas gripper y orientacion

--- build/ERC_2021_simulator/ur_kinematics/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/diego/ERC_2021/build/ERC_2021_simulator/ur_kinematics/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/diego/ERC_2021/build/ERC_2021_simulator/ur_kinematics/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
--- build/Kinematics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diego/ERC_2021/src/Kinematics
-# Build directory: /home/diego/ERC_2021/build/Kinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
