fd9753ef2ac11faa713ee56699e3f26ed8bc9d82
FelipeGJasbon <f.gomez1@uniandes.edu.co>
2021-08-06 09:15:07 -0500

    build y devel eliminados

--- build/ERC_2021_simulator/ur_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/diego/ERC_2021/src/ERC_2021_simulator/ur_driver
-# Build directory: /home/diego/ERC_2021/build/ERC_2021_simulator/ur_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
